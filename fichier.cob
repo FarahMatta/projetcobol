cd0f308fa55ac41d324458d01c1e56366f6dbf28
Farah Matta <farah_albert@hotmail.com>
2018-03-13 14:52:08 +0100

    effectuer_achat

--- fichier.cob
+++ fichier.cob
@@ -1,16 +1,16 @@
 IDENTIFICATION DIVISION.
   PROGRAM-ID.fichier.
-  
+
 ENVIRONMENT DIVISION.
 INPUT-OUTPUT SECTION.
-FILE-CONTROL.       
+FILE-CONTROL.
   SELECT fclient ASSIGN TO "clients.dat"
   ORGANIZATION INDEXED
   ACCESS MODE IS DYNAMIC
   RECORD KEY fcl_id
   ALTERNATE RECORD KEY fcl_fidele WITH DUPLICATES
   FILE STATUS IS fclient_stat.
-  
+
   SELECT fachat ASSIGN TO "achats.dat"
   ORGANIZATION INDEXED
   ACCESS MODE IS DYNAMIC
@@ -18,14 +18,14 @@ FILE-CONTROL.
   ALTERNATE RECORD KEY fa_idcmd WITH DUPLICATES
   ALTERNATE RECORD KEY fa_idart
   FILE STATUS IS fachat_stat.
-  
+
   SELECT fcmd ASSIGN TO "commandes.dat"
   ORGANIZATION INDEXED
   ACCESS MODE IS DYNAMIC
   RECORD KEY fco_id
   ALTERNATE RECORD KEY fco_idClient WITH DUPLICATES
   FILE STATUS IS fcmd_stat.
-  
+
   SELECT fart ASSIGN TO "articles.dat"
   ORGANIZATION INDEXED
   ACCESS MODE IS DYNAMIC
@@ -34,7 +34,7 @@ FILE-CONTROL.
   ALTERNATE RECORD KEY far_taille WITH DUPLICATES
   ALTERNATE RECORD KEY far_type WITH DUPLICATES
   FILE STATUS IS fart_stat.
-  
+
 DATA DIVISION.
 FILE SECTION.
 FD fclient.
@@ -45,21 +45,21 @@ FD fclient.
     02 fcl_mail PIC X(30).
     02 fcl_adresse PIC X(30).
     02 fcl_fidele PIC 9.
-    
+
 FD fachat.
   01 achatTamp.
     02 fa_id PIC 9.
     02 fa_idcmd PIC 9.
     02 fa_idart PIC 9.
     02 fa_quantite PIC 9.
-    
+
 FD fcmd.
   01 cmdTamp.
     02 fco_id PIC 9.
     02 fco_idClient PIC 9.
     02 fco_nbArticles PIC 9.
     02 fco_prix PIC 9.
-    
+
 FD fart.
   01 artTamp.
     02 far_id PIC 9.
@@ -69,7 +69,7 @@ FD fart.
     02 far_taille PIC X(30).
     02 far_type PIC X(30).
     02 far_stock PIC 9.
-    
+
 WORKING-STORAGE SECTION.
   77 fclient_stat PIC 9(2).
   77 fachat_stat PIC 9(2).
@@ -81,7 +81,8 @@ WORKING-STORAGE SECTION.
   77 Wok PIC 9(2).
   77 Wpre PIC X(30).
   77 Wnom PIC X(30).
-      
+  77 Widart PIC 9.
+
 PROCEDURE DIVISION.
 
 OPEN I-O fclient
@@ -144,15 +145,13 @@ DISPLAY '13: Gerer_stock,14:modifier_commande, 0:quitter'
         WHEN 13
                 PERFORM GERER_STOCK
         WHEN 14
-                PERFORM MODIFIER_COMMANDE               
+                PERFORM MODIFIER_COMMANDE
         END-EVALUATE
 
 END-PERFORM
 STOP RUN.
 
         AJOUT_CLIENT.
-      *>Il reste la saisie de l'id (il faut regarder selon ce que alexis a fait)
-        
         DISPLAY 'Veuillez saisir les informations de la competition'
         DISPLAY 'Nom du client'
         ACCEPT fcl_nom
@@ -167,38 +166,38 @@ STOP RUN.
           DISPLAY' Saisir 1 si le client a choisi notre programme de fidelité, 0 sinon'
           ACCEPT fcl_fidele
         END-PERFORM
-        
+
         OPEN I-O fclient
         WRITE clientTamp END-WRITE
         CLOSE fclient.
-      
-      
+
+
       SUPPRIMER_CLIENT.
-      
+
       OPEN I-O fclient
       DISPLAY 'Veuillez saisir l`identifiant du client à supprimer'
       ACCEPT Wident
       MOVE Wident TO fcl_id
       READ fclient
-      INVALID KEY 
+      INVALID KEY
         DISPLAY 'Client inexistant'
       NOT INVALID KEY
         DELETE fclient RECORD
       END-READ
       CLOSE fclient.
-      
-      
+
+
       MODIFIER_INFOCL.
-      
+
       OPEN I-O fclient
       PERFORM WITH TEST AFTER UNTIL Wok=0
         DISPLAY 'Saisissez un numero selon la modification souhaitée'
         DISPLAY '1:nom,2:prenom,3:Mail, 4:Adresse, 5:Fidelite, 0:quitter'
-        ACCEPT Wok 
+        ACCEPT Wok
         DISPLAY ' Veuillez saisir l`identifiant du client concerné'
         ACCEPT Wident
         MOVE Wident TO fcl_id
-        READ fclient 
+        READ fclient
         INVALID KEY
           DISPLAY 'Client inexistant'
         NOT INVALID KEY
@@ -227,14 +226,14 @@ STOP RUN.
             END-PERFORM
             REWRITE clientTamp END-REWRITE
         END-EVALUATE
-        END-READ 
+        END-READ
       END-PERFORM
       CLOSE fclient.
-      
-      
+
+
       RECHERCHE_FIDELITE.
-      
-      OPEN OUPUT fclient
+
+      OPEN OUTPUT fclient
       MOVE 1 TO fcl_fidele
       MOVE 0 TO Wfin
       START fclient KEY = fcl_fidele
@@ -243,7 +242,7 @@ STOP RUN.
       NOT INVALID KEY
         PERFORM WITH TEST AFTER UNTIL Wfin=1
           READ fclient NEXT
-          AT END 
+          AT END
             MOVE 1 TO Wfin
           NOT AT END
             IF fcl_fidele = 1 THEN
@@ -255,26 +254,26 @@ STOP RUN.
         END-PERFORM
       END-START
       CLOSE fclient.
-      
+
       AJOUT_ARTICLE.
-      
+
       *> id à définir encore. On récuppére l'id d'une certaine maniére puis on vérifie si elle existe ou pas.
       *>Si oui on met à jour les stock. Sinon on crée un nouveau article
       OPEN I-O fart
-      
+
       MOVE Wident TO far_id
       READ fart
       INVALID KEY
-        DISPLAY 'Veuillez saisir les informations de l'article'
-        DISPLAY ' nom de l`article'
+        DISPLAY 'Veuillez saisir les informations de l article'
+        DISPLAY 'nom de l article'
         ACCEPT far_nom
-        DISPLAY 'le prix de l`article'
+        DISPLAY 'le prix de l article'
         ACCEPT far_prix
-        DISPLAY 'couleur de l`article'
+        DISPLAY 'couleur de l article'
         ACCEPT far_couleur
-        DISPLAY 'Taille de l`article'
+        DISPLAY 'Taille de l article'
         PERFORM WITH TEST AFTER UNTIL far_type < 7
-          DISPLAY ' Type de l`article: 1:hautFemme, 2:basFemme,'
+          DISPLAY ' Type de l article: 1:hautFemme, 2:basFemme,'
           DISPLAY ' 3:AccesoireFemme, 4:HautHomme, 5:basHomme,'
           DISPLAY ' 6:AccesoireHomme'
           ACCEPT far_type
@@ -283,21 +282,19 @@ STOP RUN.
         ACCEPT far_stock
         REWRITE artTamp END-REWRITE
       NOT INVALID KEY
-        PERFORM GERER_STOCK()
+        PERFORM GERER_STOCK
       END-READ
       CLOSE fart.
-      
 
-      
-      
-      
-      
-      
-      
-        
-      
-        
-      
+      EFFECTUER_ACHAT.
 
+      DISPLAY 'Veuillez saisir les informations de l achat'
+      DISPLAY 'Veuillez saisir l id de la commande'
+      ACCEPT fa_idcmd
+      DISPLAY 'Veuillez saisir l id de l article'
+      ACCEPT Widart
+      OPEN fart INPUT
+      READ
 
-
+      DISPLAY 'Veuillez saisir la quantité acheté'
+      ACCEPT fa_quantite
